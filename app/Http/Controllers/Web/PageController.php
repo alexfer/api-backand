3fd01254bf6864c2189f943cb369ef03299cfe1b
Alexander <alexanderstyher@gmail.com>
2023-08-08 18:53:38 +0300

    login/Register implementstion

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class PageController extends Controller
 {
-    public function index() {
+
+    public function index()
+    {
         return view('welcome');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-  |--------------------------------------------------------------------------
-  | Web Routes
-  |--------------------------------------------------------------------------
-  |
-  | Here is where you can register web routes for your application. These
-  | routes are loaded by the RouteServiceProvider and all of them will
-  | be assigned to the "web" middleware group. Make something great!
-  |
- */
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/', 'App\Http\Controllers\Api\PageController@index')->name('page.index');
+Route::get('/', 'App\Http\Controllers\Web\PageController@index')->name('page.index');
+Route::get('/dashboard', 'App\Http\Controllers\Web\UserDashboardController@dashboard')->name('auth.web.dashboard');
+Route::get('/login', 'App\Http\Controllers\Web\UserDashboardController@login')->name('auth.web.login');
+Route::post('/logout', 'App\Http\Controllers\Web\UserDashboardController@logout')->name('auth.web.logout');
+Route::post('/authenticate', 'App\Http\Controllers\Web\UserDashboardController@authenticate')->name('auth.web.authenticate');
+Route::get('/register', 'App\Http\Controllers\Web\UserDashboardController@register')->name('auth.web.register');
+Route::post('/store', 'App\Http\Controllers\Web\UserDashboardController@store')->name('auth.web.store');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/users', 'App\Http\Controllers\Api\UserController@collection');
